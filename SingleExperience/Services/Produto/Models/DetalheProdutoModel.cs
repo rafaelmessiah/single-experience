d9b51c295b78f6131fcaac96081a609ae0fecd86
rafaelmessiah <rafaelmessiah@gmail.com>
2021-07-27 15:48:51 -0300

    Ajuste Service

---
+++ SingleExperience/Services/Produto/Models/DetalheProdutoModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingleExperience.Services.Produto.Models
+{
+    class DetalheProdutoModel
+    {
+        public string Descricao { get; set; }
+    }
+}
